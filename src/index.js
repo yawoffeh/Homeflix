5f491356365c580d3af62d3573e85f5f9de89386
Red <yawoffeh123@gmail.com>
2022-06-02 22:58:53 +0000

    Create first react app Homeflix

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+
+function Contacts() {
+    const phone = "+233-555-193-3257";
+    const email = "yawoffeh@gmail.com";
+    return  (
+        <div class="grey">
+            <p>Contacts</p>
+            <ul>
+                <li>Phone: {phone}</li>
+                <li>E-mail: {email}</li>
+            </ul>
+        </div>
+    );
+}
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        const url = this.props.url;
+        return (
+            <div>
+            <h1 class="red">HomeFlix</h1>
+            <p class="wid">Watch all your movies and series here</p>
+            <a href={url} class="red">Try one month free...</a>
+            <Contacts />
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <App url="#"/>,
+    document.getElementById("root")
+);
